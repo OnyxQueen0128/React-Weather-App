ef0edd8d228fcab98214233212040fc2051ecfec
OnyxQueen0128 <107898155+OnyxQueen0128@users.noreply.github.com>
2022-10-05 12:18:37 +0200

    added bootstrap and package updates

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
+import React from "react";
 import "./styles.css";
 import SearchEngine from "./SearchEngine";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <SearchEngine />
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/lysandrawj/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Lysandra Webba Jorge
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/OnyxQueen0128/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <SearchEngine />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/lysandrawj/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Lysandra Webba Jorge
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/OnyxQueen0128/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
