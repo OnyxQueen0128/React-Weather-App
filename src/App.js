21ca94db6dd938fdd3e65bbfd0b36c7c6ea1add3
OnyxQueen0128 <107898155+OnyxQueen0128@users.noreply.github.com>
2022-10-05 11:25:54 +0200

    deletions & footer
    removed redundant files & data + added footer with Github & profile links

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <SearchEngine />
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/lysandrawj/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Lysandra Webba Jorge
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/OnyxQueen0128/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
