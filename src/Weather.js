8976f8feff1833386031a99fb18d6a5634e73098
OnyxQueen0128 <107898155+OnyxQueen0128@users.noreply.github.com>
2022-10-07 13:41:48 +0200

    icon codes assigned

--- src/Weather.js
+++ src/Weather.js
@@ -15,7 +15,7 @@ export default function Weather(props) {
       temperature: Math.round(response.data.main.temp),
       humidity: response.data.main.humidity,
       wind: Math.round(response.data.wind.speed),
-      iconURL: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
     });
   }
 
